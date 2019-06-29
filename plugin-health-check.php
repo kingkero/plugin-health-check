bb720d4c7ef3770a66ac2332b901df54d09b9d23
Martin Rehberger <martin@matchwerk.de>
2019-06-29 10:04:39 +0200

    :tada: Initial commit
    Add .distignore, .editorconfig, .gitignore, .phpcs.xml.dist, composer.json, plugin-health-check.php and readme.txt with most basic standard configuration.

---
+++ plugin-health-check.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name:     Plugin Health Check
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     Adds info about installed plugins to the Site Health Check.
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     plugin-health-check
+ * Version:         0.1.0
+ */
