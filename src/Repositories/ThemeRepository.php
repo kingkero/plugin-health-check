eed49f55a54b92753d4b5a8420bbd777a1217a99
Martin Rehberger <martin@matchwerk.de>
2019-07-13 18:43:06 +0200

    :bug: Expand getActiveThemes() to get parent if exists

--- src/Repositories/ThemeRepository.php
+++ src/Repositories/ThemeRepository.php
@@ -20,15 +20,47 @@ class ThemeRepository
     }
 
     /**
-     * Get all installed plugins that are activated.
+     * Get the activated theme.
      *
      * @return Theme the currently activated theme
      */
-    public function getActiveTheme(): ?Theme
+    public function getActiveTheme(): Theme
     {
         $active = \wp_get_theme();
         return $this->getAllThemes()->filter(function ($theme) use ($active) {
             return $theme->getName() === $active->get('Name');
         })->first();
+    }
+
+    /**
+     * Get the parent theme of the activated theme.
+     *
+     * @return Theme|null the parent theme or null
+     */
+    public function getParentTheme(): ?Theme
+    {
+        $parentName = $this->getActiveTheme()->getTheme()->parent_theme;
+        if (empty($parentName)) {
+            return null;
+        }
+
+        return $this->getAllThemes()->filter(function ($theme) use ($parentName) {
+            return $theme->getName() === $parentName;
+        })->first();
+    }
+
+    /**
+     * Get all installed plugins that are activated.
+     *
+     * @return Theme the currently activated theme
+     */
+    public function getActiveThemes(): Collection
+    {
+        return \collect([
+            $this->getActiveTheme(),
+            $this->getParentTheme(),
+        ])->filter(function ($theme) {
+            return $theme !== null;
+        });
     }
 }
