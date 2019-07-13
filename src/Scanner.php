37ebf351f8393e6222d878ddc54d2aeba70bac4e
Martin Rehberger <martin@matchwerk.de>
2019-07-13 17:57:52 +0200

    :recycle: Move Scanner getPlugins() methods to PluginRepository

--- src/Scanner.php
+++ src/Scanner.php
@@ -3,34 +3,10 @@
 namespace KERO\PluginHealthCheck;
 
 use Illuminate\Support\Collection;
-use KERO\PluginHealthCheck\Models\Plugin;
 use KERO\PluginHealthCheck\Models\Theme;
 
 class Scanner
 {
-    /** @var string DIR_BLACKLIST list of directories to skip when scanning */
-    protected const DIR_BLACKLIST = [
-        '.',
-        '..',
-    ];
-
-    /**
-     * Return a 2 dimensional array of active and other plugins.
-     *
-     * @return Collection list of active and other installed plugins
-     */
-    public function getPlugins(): Collection
-    {
-        $activePlugins = \collect(\get_option('active_plugins'));
-
-        return \collect(\get_plugins())->mapToGroups(function ($data, $file) use ($activePlugins) {
-            $key = $activePlugins->contains($file)
-                ? 'active'
-                : 'other';
-            return [$key => new Plugin($data, $file)];
-        });
-    }
-
     /**
      * Return a 2dimensional array of the active and other themes.
      *
