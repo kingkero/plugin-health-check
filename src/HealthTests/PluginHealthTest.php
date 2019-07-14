a06e177ac260f4ad7c61cd1ea44658543921f5a0
Martin Rehberger <martin@matchwerk.de>
2019-07-14 15:35:35 +0200

    :sparkles: Proper async Site Health Testk working

--- src/HealthTests/PluginHealthTest.php
+++ src/HealthTests/PluginHealthTest.php
@@ -7,51 +7,21 @@ use stdClass;
 
 class PluginHealthTest
 {
+    public const TEST = 'plugin_updates';
+
     public static function add(array $tests): array
     {
-        $tests['async']['caching_plugin'] = [
-            'label' => __('My Caching Test'),
-            'test'  => 'testPlugins',
+        $tests['async'][Plugin::PREFIX . self::TEST] = [
+            'label' => __('Plugin updates available', 'plugin-health-check'),
+            'test'  => Plugin::PREFIX . self::TEST,
         ];
         return $tests;
     }
 
-    public static function test(): stdClass
+    public static function test()
     {
-        return (object) [
-            'status' => 'recommended',
-            'message' => 'foo',
-        ];
-
-        $result = array(
-            'label'       => __('Caching is enabled'),
-            'status'      => 'recommended',
-            'badge'       => array(
-                'label' => __('Performance'),
-                'color' => 'orange',
-            ),
-            'description' => sprintf(
-                '<p>%s</p>',
-                __('Caching can help load your site more quickly for visitors.')
-            ),
-            'actions'     => '',
-            'test'        => 'caching_plugin',
-        );
-
-        /* if ( ! myplugin_caching_is_enabled() ) {
-            $result['status'] = 'recommended';
-            $result['label'] = __( 'Caching is not enabled' );
-            $result['description'] = sprintf(
-                '<p>%s</p>',
-                __( 'Caching is not currently enabled on your site. Caching can help load your site more quickly for visitors.' )
-            );
-            $result['actions'] .= sprintf(
-                '<p><a href="%s">%s</a></p>',
-                esc_url( admin_url( 'admin.php?page=cachingplugin&action=enable-caching' ) ),
-                __( 'Enable Caching' )
-            );
-        } */
-
-        return $result;
+        check_ajax_referer('health-check-site-status');
+        echo 'foo';
+        die;
     }
 }
